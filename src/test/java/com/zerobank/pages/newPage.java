2938f31adb53a7728e4a3b38be44b5cbbbaf6b3a
belletrax <elifalan@yahoo.com>
2020-12-01 16:29:45 +0000

    newly updated

--- src/test/java/com/zerobank/pages/newPage.java
+++ src/test/java/com/zerobank/pages/newPage.java
@@ -3,5 +3,8 @@ package com.zerobank.pages;
 public class newPage {
     public static void main(String[] args) {
         System.out.println("comes from newPage");
+
+
+        //asdafgsdsfsfsdf
     }
 }
